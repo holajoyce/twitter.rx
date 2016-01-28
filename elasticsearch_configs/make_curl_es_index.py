3b1e556fc6aaea5ffed85306a3a22f972219ee1b
joyce chan <joyce@selfcarecatalysts.com>
2016-01-28 15:37:21 -0800

    completed script to update indicies

--- elasticsearch_configs/make_curl_es_index.py
+++ elasticsearch_configs/make_curl_es_index.py
@@ -1,34 +1,62 @@
 import os
 import sys
-
+from subprocess import Popen, PIPE
 
 """
 this script create curl files for elasticsearch
 """
-def create_curl_index_file():
-    mydir = os.path.dirname(os.path.realpath(sys.argv[0]))
-    
+mydir = os.path.dirname(os.path.realpath(sys.argv[0]))
+server = "ec2-52-88-7-3.us-west-2.compute.amazonaws.com"
+
+
+def get_list_of_drug_comps():
+    drug_companies = None
     drug_comps_file = open(mydir+"/all_drug_companies.txt","r")
-    
     with drug_comps_file as f:
         drug_companies = f.readlines()
-        
+    drug_comps_file.close()
+    return drug_companies
+    
+    
+def create_curl_index_file():
+    drug_companies = get_list_of_drug_comps()
     curl_templ_file = open(mydir+"/curl_templ","r")
     curl_templ_str = curl_templ_file.read()
-    print(curl_templ_str)
+    #print(curl_templ_str)
     
     for drug_company in drug_companies:
-        drug_company= drug_company.rstrip("\n")
-        curl_templ_str2 = curl_templ_str.replace("ZZZ",drug_company)
-        curl_file = open(mydir+"/out/pharma_"+drug_company+".curl",'w')
-        curl_file.write(curl_templ_str2)
-        curl_file.close()
-        
-    
+        if drug_company.strip()!="":
+            drug_company= drug_company.rstrip("\n")
+            curl_templ_str2 = curl_templ_str.replace("ZZZ","pharma_"+drug_company)
+            curl_file = open(mydir+"/out/pharma_"+drug_company+".curl",'w')
+            curl_file.write("curl -X PUT http://"+server+":9200/_template/pharma_"+drug_company+" -d '"+curl_templ_str2+"'"+' -H "Accept: application/json"')
+            curl_file.close()
     curl_templ_file.close()
-    drug_comps_file.close()
-    
+
+
+def create_indexes_helper(drug_comp=None):
+    file_path = mydir+"/out/pharma_"+drug_comp+".curl"
+    print(file_path)
+    curl_file = open(file_path,'r')
+    curl_file_str = curl_file.read()
+    print(curl_file_str)
+    sproc = Popen(curl_file_str, stdout=PIPE, shell=True)
+    out, err = sproc.communicate()
+    print(out)
+
+
+def create_indexes(drug_comp=None):
+    if(drug_comp!=None):
+        create_indexes_helper(drug_comp)
+    else:
+        drug_companies = get_list_of_drug_comps()
+        for drug_company in drug_companies:
+            if drug_company.strip()!="" and drug_company is not None:
+                create_indexes_helper(drug_company.rstrip('\n'))
+
 
 
 if __name__=="__main__":
-    create_curl_index_file()
+    #create_curl_index_file()
+    #create_indexes("abbvie_inc")
+    create_indexes()
