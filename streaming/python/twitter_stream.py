733fc45a8384df6486a6376864bc4f6353af59d1
joyce chan <joyce@selfcarecatalysts.com>
2016-01-29 11:29:36 -0800

    up

--- streaming/python/twitter_stream.py
+++ streaming/python/twitter_stream.py
@@ -16,8 +16,8 @@
 #
 
 """
- Counts words in UTF8 encoded, '\n' delimited text directly received from Kafka in every 2 seconds.
- Usage: direct_kafka_wordcount.py <broker_list> <topic>
+ Counts words in UTF8 encoded, '\n' delimited text received from the network every second.
+ Usage: kafka_wordcount.py <zk> <topic>
 
  To run this on your local machine, you need to setup Kafka and create a producer first, see
  http://kafka.apache.org/documentation.html#quickstart
@@ -25,9 +25,10 @@
  and then run the example
     `$ bin/spark-submit --jars \
       external/kafka-assembly/target/scala-*/spark-streaming-kafka-assembly-*.jar \
-      examples/src/main/python/streaming/direct_kafka_wordcount.py \
-      localhost:9092 test`
+      examples/src/main/python/streaming/kafka_wordcount.py \
+      localhost:2181 test`
 """
+from __future__ import print_function
 
 import sys
 
@@ -35,19 +36,22 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
+        exit(-1)
 
+    sc = SparkContext(appName="stream_tagger")
+    ssc = StreamingContext(sc, 1)
 
-sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-ssc = StreamingContext(sc, 2)
+    zkQuorum, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a+b)
+    counts.pprint()
 
-brokers, topic = sys.argv[1:]
-kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-lines = kvs.map(lambda x: x[1])
-counts = lines.flatMap(lambda line: line.split(" ")) \
-    .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a+b)
-counts.pprint()
-
-ssc.start()
-ssc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
 
