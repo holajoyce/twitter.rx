39cf0ccce325c8a3182c85e9a7442ee6df8de7a8
joyce chan <joyce@selfcarecatalysts.com>
2016-01-29 11:19:05 -0800

    up

--- streaming/python/twitter_stream.py
+++ streaming/python/twitter_stream.py
@@ -1,24 +1,53 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+ Counts words in UTF8 encoded, '\n' delimited text directly received from Kafka in every 2 seconds.
+ Usage: direct_kafka_wordcount.py <broker_list> <topic>
+
+ To run this on your local machine, you need to setup Kafka and create a producer first, see
+ http://kafka.apache.org/documentation.html#quickstart
+
+ and then run the example
+    `$ bin/spark-submit --jars \
+      external/kafka-assembly/target/scala-*/spark-streaming-kafka-assembly-*.jar \
+      examples/src/main/python/streaming/direct_kafka_wordcount.py \
+      localhost:9092 test`
+"""
+
+import sys
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from pyspark.sql import SQLContext, Row
-from pyspark import SparkConf
-from pyspark.sql.types import *
-import json
 
-sc = SparkContext(appName="TwitterApp")
+
+
+sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
 ssc = StreamingContext(sc, 2)
-sqlContext = SQLContext(sc)
 
-
-kafkaStream = KafkaUtils.createStream(ssc, "ip-172-31-1-136:2181,ip-172-31-1-137:2181,ip-172-31-1-133:2181,ip-172-31-1-139:2181", "spark-streaming consumer", {"reddit4": 1})
-lines = kafkaStream.map(lambda x: x[1])
-
-# {"name":"t1_c8770z6","author_flair_css_class":null,"author":"corey3","author_flair_text":null,"ups":1,"id":"c8770z6","edited":false,"retrieved_on":1431145410,"score_hidden":false,"gilded":0,"downs":0,"body":"also try to get ADHD medication.","controversiality":0,"subreddit_id":"t5_2qnwb","distinguished":null,"parent_id":"t1_c85yez1","subreddit":"ADHD","archived":true,"score":1,"link_id":"t3_17itl1"}
+brokers, topic = sys.argv[1:]
+kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+lines = kvs.map(lambda x: x[1])
 counts = lines.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a+b)
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a+b)
 counts.pprint()
 
 ssc.start()
 ssc.awaitTermination()
+
