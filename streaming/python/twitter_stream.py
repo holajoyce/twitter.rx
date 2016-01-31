518fdc9bb44de3377a24fc0fb084fd1c1cddbd97
Ubuntu <ubuntu@ip-172-31-1-134.us-west-2.compute.internal>
2016-01-31 00:58:06 +0000

    up

--- streaming/python/twitter_stream.py
+++ streaming/python/twitter_stream.py
@@ -24,7 +24,10 @@ from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import SQLContext, Row
 import json
 import requests
+from time import gmtime, strftime
+import unicodedata
 
+# line="""{"subreddit_id":"t5_2yljs","score":0,"edited":false,"name":"t1_cekta7f","author_flair_css_class":"default","author":"Kimera25","parent_id":"t1_ceks74z","link_id":"t3_1uqrin","retrieved_on":1431859483,"score_hidden":false,"author_flair_text":"360 100%","subreddit":"chiliadmystery","downs":0,"removal_reason":null,"controversiality":0,"id":"cekta7f","ups":0,"gilded":0,"distinguished":null,"body":"I just tried it and nothing, it's a half moon on a tuesday, no rain, i'll try it with a thunderstorm next time. this got me thinking that the mural could have been painted by the Altruists and each of the X's represents a sacrifice and you have to do the fifth one, I guess on chop. random theory built off all this.","archived":true}"""
 # https://github.com/apache/spark/blob/master/examples/src/main/python/streaming/sql_network_wordcount.py
 # https://github.com/willzfarmer/TwitterPanic/blob/master/python/analysis.py
 # https://raw.githubusercontent.com/rustyrazorblade/killranalytics/intro_streaming_python2/killranalytics/spark/raw_event_stream_processing.py
@@ -56,31 +59,32 @@ def tfunc_transform_data_stream(t, rdd):
   :param rdd: rdd
       Current rdd we're mapping to
   """
-  return rdd.flatMap(lambda x: transform_data_stream(x))
+  return rdd.flatMap(lambda x: transform_data_stream)
 
 
 
-def transform_data_stream(line):
-  print("========= transform data stream method starting %s =========" % str(time))
-  print(line)
-  line_enriched  = requests.post(query_url,data=line)
-  yield line_enriched
+def transform_data_stream(json_line):
+  print("............. transform data stream method starting "+strftime("%Y-%m-%d %H:%M:%S", gmtime())+" .............")
+  # lineTuple.pprint()
+  line_enriched_json = requests.post(tagger_url,data=json.dumps(json_line)).json()
+  jstr = json.dumps(line_enriched_json)
+  print(jstr)
+  #line_enriched = line_enriched.encode('ascii','ignore')
+  #lineTuple = (lineTuple[0],jstr)
+  print("............. end data stream method starting "+strftime("%Y-%m-%d %H:%M:%S", gmtime())+" ............." )
+  #yield line_enriched.text.encode("utf-8")
+  yield jstr
   # data      = [('language', 'en'), ('locations', '-130,20,-60,50')]
   # # query_url = config.url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in data])
   # post = json.loads(line.decode('utf-8'))
   # print(line)
-  print("========= end data stream method starting %s =========" % str(time))
-
-
-
-def parse_json(v):
-  return (None, json.loads(v))
-
 
 
 def process(time, rdd):
   print("========= process method starting %s =========" % str(time))
   try:
+    # change the rdd to a tagged one
+    #rdd = requests.post(tagger_url,data=json.dumps(rdd)).json()
     rowRdd = rdd.map(lambda w: Row(author=w['user_screen_name'], body=w['text'], created_at_utc=w['timestamp_ms'][0:9]))
     df = getSqlContextInstance(rdd.context).createDataFrame(rowRdd) 
     df.registerTempTable("df")
@@ -94,9 +98,22 @@ def process(time, rdd):
 
 
 
-stream = stream.transform(tfunc_transform_data_stream)
-parsed = stream.map(lambda (k,v): json.loads(v))
-parsed.foreachRDD(process)
+# jsons_stream = stream.map(lambda x: json.loads(x[1])) # make new stream
+# jsons_stream = stream.map(tag) # make new stream
+#parsed = jsons_stream.transform(tfunc_transform_data_stream)
+def printRdd(x):
+  print(json.dumps(x.take(2)))
+  print(x)
+
+# lines = stream.map(lambda x: json.loads(x[1]))
+lines = stream.map(lambda x: requests.post(tagger_url,data=json.dumps(json.loads(x[1])) ) )
+lines.foreachRDD(printRdd)
+
+# stream = stream.transform(tfunc_transform_data_stream)
+# parsed = stream.map(lambda (k,v): json.loads(v))
+#stream = stream.transform(tfunc_transform_data_stream)
+
+# parsed.foreachRDD(process)
 ssc.start()
 ssc.awaitTermination()
 
